214c241e2c1723e5f484680e5522b66efbac02a7
kolapraneeth22 <kola.praneeth2020@vitstudent.ac.in>
2024-10-20 00:04:25 +0530

    training added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'
 from chickendisease import logger
 from chickendisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chickendisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chickendisease.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -24,6 +25,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
