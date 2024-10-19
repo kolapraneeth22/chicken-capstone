ae4232e1f369c6869007d0ee7c9836257be7c58a
kolapraneeth22 <kola.praneeth2020@vitstudent.ac.in>
2024-10-19 21:23:14 +0530

    prepare callbacks added

--- src/chickendisease/config/configuration.py
+++ src/chickendisease/config/configuration.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 from chickendisease.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig)
 
 # Add the 'src' directory to the PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), 'src')))
@@ -53,3 +54,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
