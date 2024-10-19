539bc4ab7a02bf5b3a5b05aa8748b6d12d87b14a
kolapraneeth22 <kola.praneeth2020@vitstudent.ac.in>
2024-10-19 17:48:04 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 from chickendisease import logger
 from chickendisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chickendisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -16,3 +17,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
